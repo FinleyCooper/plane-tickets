f65782a3d51238dd841b26fa62bd24f8dfddaeaf
Finley Cooper <contact@finleycooper.co.uk>
2021-11-21 11:05:06 +0000

    Changed try block

--- App/aircraft/admin_manager.py
+++ App/aircraft/admin_manager.py
@@ -1,7 +1,12 @@
 from sqlite3 import OperationalError
 
 def create_plane(name, seat_length, seat_width, connection):
-    connection.execute(f"CREATE TABLE {name}(id VARCHAR(3), passengerID VARCHAR(30), passengerInformation VARCHAR(255), class VARCHAR(10))")
+    try:
+        connection.execute(f"CREATE TABLE {name}(id VARCHAR(3), passengerID VARCHAR(30), passengerInformation VARCHAR(255), class VARCHAR(10))")
+    
+    except OperationalError:
+        print(f"The table {name} already exists")
+        return None
 
     seat_values = []
     alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O']
@@ -10,10 +15,8 @@ def create_plane(name, seat_length, seat_width, connection):
         for j in range(seat_length):
             seat_values.append(f"('{alphabet[i] + str(j+1)}', NULL, NULL, 'economy')")
     
-    try:
+
         connection.execute(f'INSERT INTO {name} (id, passengerID, passengerInformation, class) VALUES {",".join(seat_values)}')
         connection.commit()
-    except Exception as e:
-        print(type(e))
-        print(f"The table {name} already exists")
+
         
