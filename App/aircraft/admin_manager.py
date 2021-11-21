804056d2cea7df17842355f212c06f6c59e68d89
Finley Cooper <contact@finleycooper.co.uk>
2021-11-21 11:01:07 +0000

    Added try catch for dupe tables

--- App/aircraft/admin_manager.py
+++ App/aircraft/admin_manager.py
@@ -1,3 +1,5 @@
+from sqlite3 import OperationalError
+
 def create_plane(name, seat_length, seat_width, connection):
     connection.execute(f"CREATE TABLE {name}(id VARCHAR(3), passengerID VARCHAR(30), passengerInformation VARCHAR(255), class VARCHAR(10))")
 
@@ -7,6 +9,10 @@ def create_plane(name, seat_length, seat_width, connection):
     for i in range(seat_width):
         for j in range(seat_length):
             seat_values.append(f"('{alphabet[i] + str(j+1)}', NULL, NULL, 'economy')")
-
-    connection.execute(f'INSERT INTO {name} (id, passengerID, passengerInformation, class) VALUES {",".join(seat_values)}')
-    connection.commit()
+    
+    try:
+        connection.execute(f'INSERT INTO {name} (id, passengerID, passengerInformation, class) VALUES {",".join(seat_values)}')
+        connection.commit()
+    except OperationalError:
+        print(f"The table {name} already exists")
+        
